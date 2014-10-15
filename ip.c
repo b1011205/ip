b0159c600a3237dc221f98a399b465a8a955d2d7
Hirotaka_Hanada <b1011205@gmail.com>
2014-10-15 10:06:09 +0900

    配列を*を基準として左と右に分ける処理を書きました

--- ip.c
+++ ip.c
@@ -1,12 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int suji(char a[256]){
+
+int i = 0;
+for(i = 0; i < strlen(a); i++){
+
+
+}
+
+return a;
+
+}
 
 int main(void){
 
-char a[256];
 
-scanf("%s",a);
+	char moto[256];
+	char sahen[256],uhen[256];
 
-printf("%s\n",a);
+	scanf("%s",moto);
 
-return 0;
+	//*より左辺を切り取りsahenに代入
+	int i = 0, asta = -1;
+	for(i = 0; i < strlen(moto); i++){
+		if(moto[i] != '*' && asta == -1){
+			sahen[i] = moto[i];
+			printf("%s\n%s\n", moto, sahen);
+
+		}
+		if(moto[i] == '*'){
+			asta = i;
+		}
+	}
+	//*より右辺を切り取りuhenに代入
+	int k = 0, j = asta+1;
+	for(j = asta+1; j<strlen(moto); j++ ){
+		uhen[k] = moto[j];
+		printf("%s\n%s\n",moto, uhen);
+		k++;
+	}
+
+	int uhensu[256],sahensu[256];
+
+	uhensu = suji(uhen);
+	sahensu = suji(sahen);
+
+	return 0;
 }
